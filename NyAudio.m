69b5cda08d9aa30c3ad0b90681947cdf3113243d
Haley Alexander <15005204+hal00alex@users.noreply.github.com>
2018-04-26 07:25:12 -0500

    Update NyAudio.m

--- NyAudio.m
+++ NyAudio.m
@@ -1,1 +1,10 @@
-%Next Lesson to be Posted Later
+fs = 10;
+t = 0:1/fs:20; 
+y = sin(1.8*2*pi*t) + (2.1*2*pi*t); 
+plot (t,y);
+yfft = fft(y);
+n = numel(y); 
+f = 0:fs/n:fs*(n-1)/2;
+plot (f, abs(yfft)); 
+%This creates a plot to demostrate Nyquist Theorem
+%So there are two high peaks which shows that you need to sample at least twice
